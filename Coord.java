134275ec1b11a10fcf35e075fa4dccb09223cdb2
AlexandreBler <alexandreblerreau@icloud.com>
2017-03-05 16:17:14 +0100

    Add files via upload
    uesh, classe obstacle + classe Coord

---
+++ Coord.java
@@ -0,0 +1,41 @@
+
+/**
+ * 
+ * Classe Coord - traitement des coordonnées 
+ * 
+ */
+package jalon1;
+
+public class Coord {
+    
+    private int positionX;
+    private int positionY;
+    
+    public Coord(int positionX,int positionY){
+        
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+    
+    public int getPositionX(){
+        
+        return this.positionX;
+    }
+    
+    public void setPositionX(int x){
+            
+        this.positionX = x;
+    }
+    
+    public int getPositionY(){
+        
+        return this.positionY;
+    }
+        
+    public void setPositionY(int y){
+        
+        this.positionY = y;
+    }
+    
+    
+}
