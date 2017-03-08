09cd2e8b999f8a3385ebbf3f4beb200fcb47dde2
Doudou742 <geoffreydeschamps@hotmail.fr>
2017-03-08 10:37:08 +0100

    Début de la classe plateau
    pour ceux qu'ils veulent jeter un oeil

---
+++ Plateau.java
@@ -0,0 +1,67 @@
+package Plateaux;
+
+
+import java.util.ArrayList;
+
+public class Plateau {
+	final private int taille = 20; 
+	private ArrayList<Robot> listeRobot;
+	private ArrayList<Element> listeElement;
+	private ArrayList<Mine> listeMine;
+	private char[][] plateau;
+	
+	public Plateau(){
+		this.plateau = new char[taille][taille];
+		this.listeRobot = new ArrayList<>();
+		this.listeElement = new ArrayList<>();
+		this.listeMine = new ArrayList<>();
+		this.listeMine = new ArrayList<>();
+	}
+	
+	public Plateau(int nbLigne, int nbColonne) {
+		this.plateau = new char[nbLigne][nbColonne];
+		this.listeRobot = new ArrayList<>();
+		this.listeElement = new ArrayList<>();
+		this.listeMine = new ArrayList<>();
+		this.listeMine = new ArrayList<>();
+	}
+
+	public ArrayList<Robot> getListeRobot() {
+		return listeRobot;
+	}
+
+	public void setListeRobot(ArrayList<Robot> listeRobot) {
+		this.listeRobot = listeRobot;
+	}
+
+	public ArrayList<Element> getListeElement() {
+		return listeElement;
+	}
+
+	public void setListeElement(ArrayList<Element> listeElement) {
+		this.listeElement = listeElement;
+	}
+
+	public ArrayList<Mine> getListeMine() {
+		return listeMine;
+	}
+
+	public void setListeMine(ArrayList<Mine> listeMine) {
+		this.listeMine = listeMine;
+	}
+
+	public char[][] getPlateau() {
+		return plateau;
+	}
+
+	public void setPlateau(char[][] plateau) {
+		this.plateau = plateau;
+	}
+
+	public int getTaille() {
+		return taille;
+	}
+	
+	
+	
+}
