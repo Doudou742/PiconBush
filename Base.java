064b34d6aba4cfe528351c7b9c09ce3fb705c851
Nerdolphor <raphael_62@hotmail.fr>
2017-03-23 17:14:40 +0100

    Add files via upload
    Class Base
    Comprend pas la methode ajoute, elle modifie donc la valeur d'equipe, comme un setter.

---
+++ Base.java
@@ -0,0 +1,43 @@
+package personnage;
+
+public class Base extends Cellule{
+
+	private int equipe;
+	
+	public Base(int largeur, int hauteur, int equipe){
+		// Récupère le constructeur de la classe abstraite Cellule
+		super(largeur, hauteur);
+		this.equipe = equipe;
+	}
+	
+	// Getter/Setter
+	public int getEquipe(){
+		return this.equipe;
+	}
+	
+	public void setEquipe(int equipe){
+		this.equipe=equipe;
+	}
+	
+	// Methodes 
+	
+	// Récupère methode abstraite
+	public void ajoute(int equipe){
+		this.equipe=equipe;
+	}
+	
+	public void videCase(){
+		this.unRobot = null;
+		this.base=1;
+		this.mine=0;
+	}
+	
+	// Déplace le robot passé en paramètre sur la base.
+	public void deplaceSur(Robot robot){
+		// Change les coordonnées du robot pour mettre les coordonnées de la base a la place.
+		this.unRobot.setCoordonnee(this.getCoordonnees());
+	}
+	
+	
+	
+}
