680d7bd1c81ae9a450555e28479473d996f5cf8b
AlexandreBler <alexandreblerreau@icloud.com>
2017-03-24 13:50:26 +0100

    Add files via upload

---
+++ Attaque.java
@@ -0,0 +1,20 @@
+package personnages;
+
+import plateau.Coord;
+
+public class Attaque extends Action {
+	
+	public Attaque(Robot unRobot, Coord uneCoord){
+	super(unRobot,uneCoord);	
+		
+	}
+
+	@Override
+	public void agit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
