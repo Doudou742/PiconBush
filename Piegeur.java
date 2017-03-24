5cc42e1a5965e21ff9d45e31c1532249004bbe7e
Doudou742 <geoffreydeschamps@hotmail.fr>
2017-03-24 19:21:46 +0100

    Add files via upload

---
+++ Piegeur.java
@@ -0,0 +1,59 @@
+package personnages;
+
+import java.util.List;
+
+public class Piegeur extends Robot{
+	private static int deplacement = 1;
+	private static int coutAction = -2;
+	private static int coutDeplacement = -2;
+	private static int degat = -2;
+	private static String type = "Piegeur";
+	
+	public Piegeur(Vue vue, int abscisse, int ordonnee, int equipe) {
+		super(vue, abscisse, ordonnee, equipe);
+		super.setEnergie(50);
+	}
+	
+	public boolean peutTirer(){
+		return super.getEnergie() > coutAction;
+	}
+	
+	
+	
+	public int getDeplacement() {
+		return deplacement ;
+	}
+	
+	public void setDeplacement(int deplacement) {
+		this.deplacement = deplacement;
+	}
+	
+	public int getCoutAction() {
+		return this.coutAction;
+	}
+	
+	public int getCoutDep() {
+		return this.coutDeplacement;
+	}
+
+	public int getDegat() {
+		return this.degat;
+	}
+	
+	public String getType() {
+		return this.type;
+	}
+
+
+	@Override
+	public List<Coord> getDeplacements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean cibeVide(Cellule cellule) {
+		return cellule.estLibre();
+	}
+	
+}
