c4d8ff380b3acd20afb431bfbfe7c3dc7180c020
LouisDblcrt <louis.dbl@hotmail.fr>
2017-03-28 16:20:08 +0200

    Add files via upload

--- Saisie.java
+++ Saisie.java
@@ -1,29 +1,43 @@
-
+package personnages;
 import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 	
 	public class Saisie {
 
-		private String message;
+		static String message;
 		private Scanner unScanner;
+		static JOptionPane jop1;
 		
 		public Saisie(String message){
 			this.message = message;
+			jop1 = new JOptionPane();
 			unScanner = new Scanner(System.in);
 			
 		}
 		
-		public String SaisieString(){			
-			System.out.println(message + ": ");
-			return unScanner.next();
+		public static String SaisieString(){			
+			//System.out.println(message + ": ");
+			return  jop1.showInputDialog(null,message,"Mettez nous 20 svp");
 		}
 		
-		public int SaisieInt(){
-			System.out.println(message + ": ");
-			return unScanner.nextInt();
+		public static int SaisieInt(){
+			//System.out.println(message + ": ");
+			String mess=jop1.showInputDialog(null,message,"42");
+			return Integer.parseInt(mess);
 		}
 		
 		public void setMessage(String message){
 			
 			this.message = message;
+		}
+		
+		public static void main(String [] args){
+			Saisie tank = new Saisie("Quel robot voulez vous bouger ?");
+			String a ="";
+			a=SaisieString();
+			int b=SaisieInt();
+			System.out.println(a + b);
 		}
 }
